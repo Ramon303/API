d18e7d7ce0a7c6804f418d07f81ffd10a52877a1
Ramon303 <a01385671@tec.mx>
2026-04-10 22:23:42 -0600

    Actividad

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,32 +1,36 @@
-import { db_connect } from "../utils/db.js"
+// import { user } from "pg/lib/defaults";
+// import { db_connect } from "../utils/db.js"
 
+const MOCK_USERS = [
+    {id: 1, email: "admin@test.com", password: "123", name: "Administrador"},
+    {id: 2, email: "david@tec.mx", password: "password123", name: "David Rangel"}
+]
 export const login = async (req, res) => {
 
     const { email, password } = req.body
 
+    console.log("Intento de login con: ", email);
+
     try {
+        
+        const userFound = MOCK_USERS.find(user => user.email === email && user.password === password);
 
-        const sql = await db_connect()
-
-        const result = await sql.query(
-            "SELECT * FROM users WHERE email=$1 AND password=$2",
-            [email, password]
-        )
-
-        if (result.rows.length === 0) {
+        if(!userFound){
             return res.status(401).json({
-                message: "Usuario o contraseña incorrectos"
-            })
+                message: "Usuario o contraseña incorrectos (simulado)"
+            });
         }
 
         res.json({
             message: "Login exitoso",
-            user: result.rows[0]
-        })
+            user: {
+                id: userFound.id,
+                email: userFound.email,
+                name: userFound.name
+            }
+        });
 
     } catch (error) {
-
-        console.log(error)
 
         res.status(500).json({
             message: "Error en el servidor"
--- index.js
+++ index.js
@@ -11,17 +11,17 @@ import routes from "./routes/index.routes.js"
 console.log("HOST:", process.env.DB_HOST)
 console.log("PORT:", process.env.DB_PORT)
 
-const sql = await db_connect()
+// const sql = await db_connect()
 
-const result = await sql.query("SELECT 1")
-console.log(result.rows[0])
+// const result = await sql.query("SELECT 1")
+// console.log(result.rows[0])
 
 const app = express()
 
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
-
+app.use(express.static('../public')); 
 app.use(routes)
 
 app.listen(8000, () => {
